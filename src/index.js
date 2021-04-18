97c79fd259edb611e79d1ae959d22b63a288d6d4
nodejsdeveloperskh <node.js.developers.kh@gmail.com>
2021-04-18 11:07:54 +0430

    fix (mongo): connection string

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const mongoose = require('mongoose');
 
 mongoose
     .connect(
-        `mongodb://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_HOST}:${process.env.MONGODB_PORT}/${process.env.MONGODB_DATABASE}`,
+        `mongodb://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_HOST}:${process.env.MONGODB_EXPOSED_PORT}/${process.env.MONGODB_DATABASE}`,
         {
             autoReconnect: true,
             reconnectTries: 30,
