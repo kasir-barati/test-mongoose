db795dab2cd1d15fa01321d793bbf0975419cc28
nodejsdeveloperskh <node.js.developers.kh@gmail.com>
2021-04-19 15:51:56 +0430

    add (ticket): get-jwt-token route

--- src/routers/user.router.js
+++ src/routers/user.router.js
@@ -1,8 +1,40 @@
 // @ts-check
+const jwt = require('jsonwebtoken');
 const router = require('express').Router();
+
+const {
+    envs: { common: commonConfigs, jwt: jwtConfigs },
+} = require('../configs/env');
 
 const userUserController = require('../controllers/user/user');
 const userAdminController = require('../controllers/admin/user');
+
+if (commonConfigs.nodeEnv === 'development') {
+    router
+        .route('/api/dev/get-jwt-token')
+        .all(/* permission checking */)
+        .post((req, res, next) => {
+            const { userId, isAdmin } = req.body;
+
+            const token = jwt.sign(
+                {
+                    ...(isAdmin
+                        ? { admin: true }
+                        : { contractor: true }),
+                    id: userId,
+                },
+                isAdmin
+                    ? jwtConfigs.adminSecret
+                    : jwtConfigs.userSecret,
+            );
+
+            res.status(200).json({
+                successful: true,
+                error: null,
+                token,
+            });
+        });
+}
 
 router
     .route('/api/users/who-am-i')
