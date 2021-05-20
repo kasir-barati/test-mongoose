e22ac691d427a26d2ba2ba97c2ffde1ee086c302
Kasir <node.js.developers.kh@gmail.com>
2021-05-20 10:45:03 +0430

    update (index.js): implement simplest tecnique for pagination

--- src/index.js
+++ src/index.js
@@ -6,15 +6,37 @@ require('dotenv').config({
 
 const mongoose = require('mongoose');
 
+const { PostModel } = require('./post.model');
+
 mongoose
     .connect(
         `mongodb://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_HOST}:${process.env.MONGODB_PORT}/${process.env.MONGODB_DATABASE}`,
         {
             autoReconnect: true,
             reconnectTries: 30,
+            useNewUrlParser: true,
+            useCreateIndex: true,
+            useFindAndModify: true,
+            useUnifiedTopology: true,
         },
     )
-    .then((connection) => {
-        console.log(connection);
+    .then(async (connection) => {
+        let limit = 100,
+            offset = 0,
+            posts;
+
+        do {
+            console.log(limit, offset);
+            posts = await PostModel.find()
+                .limit(limit)
+                .skip(limit * offset)
+                .exec();
+            /**@type {import('./post-type').PostType} */
+            let post;
+            for (post of posts) {
+                // ...
+                offset++;
+            }
+        } while (posts.length > 0);
     })
     .catch(console.error);
